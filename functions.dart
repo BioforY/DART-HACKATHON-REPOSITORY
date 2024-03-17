ad0924748646a1ea2fd36c385c402079a5ab8b18
Gloria Khamaya <gloriamaleche@gmail.com>
2024-03-17 22:58:54 +0300

    Hackathon lvl1

--- functions.dart
+++ functions.dart
@@ -1,2 +1,16 @@
-## Program 2: Perform Mathematical Operations with Functions
-Write a Dart program that performs two mathematical operations using functions.
+void main() {
+  double num1 = 20;
+  double num2 = 50;
+
+  double result1 = addNumbers(num1, num2);
+  double result2 = multiplyNumbers(num1, num2);
+
+  print("Result of addition: $result1");
+  print("Result of multiplication: $result2");
+}
+  double addNumbers(double a, double b){
+    return a+b;
+  }
+  double multiplyNumbers(double a, double b){
+    return a*b;
+  }
---
+++ program1.dart
@@ -0,0 +1,7 @@
+void main() {
+  String name = "Gloria Maleche";
+  int age = 24;
+  String school = "University of Nairobi";
+  String hobby = "sleeping";
+  print("My name is $name. I am $age years old. I attend $school, and my favourite hobby is $hobby. Sue me. ");
+}
